e8df2d65aa523cd0c999ed48312c4812d830e6af
piyush-xo <kpiyush381@gmail.com>
2023-10-13 22:15:35 +0530

    feat: added buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import editIcon from "./assets/edit.svg";
+import deleteIcon from "./assets/delete.svg";
 import "./App.css";
 
 function App() {
@@ -12,36 +14,70 @@ function App() {
       .then((res) => setContacts(res));
   }, []);
 
+  const handleAdd = () => {
+    console.log("adding");
+  };
+
+  const handleEdit = (id) => {
+    console.log("editing " + id);
+  };
+
+  const handleDelete = (id) => {
+    console.log("deleting " + id);
+  };
+
   return (
-    <main className="container">
-      {!contacts ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          
-          <table className="contactsTable">
-            <thead>
-              <tr>
-                <th style={{width:"10%"}}>ID</th>
-                <th style={{width:"25%"}}>Name</th>
-                <th style={{width:"25%"}}>Email/Phone</th>
-                <th style={{width:"40%"}}>About</th>
-              </tr>
-            </thead>
-            <tbody>
-              {contacts.map((contact, index) => (
-                <tr key={contact.id}>
-                  <td style={{width:"10%"}}>{index+1}</td>
-                  <td style={{width:"25%"}}>{contact.name}</td>
-                  <td style={{width:"25%"}}>{contact.phone} - {contact.email}</td>
-                  <td style={{width:"40%"}}>{contact.about}</td>
+    <div className="container">
+      <>
+        {!contacts ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            <table className="contactsTable">
+              <thead>
+                <tr>
+                  <th style={{ width: "8%" }}>S.No.</th>
+                  <th style={{ width: "23%" }}>Name</th>
+                  <th style={{ width: "24%" }}>Email/Phone</th>
+                  <th style={{ width: "38%" }}>About</th>
+                  <th style={{ width: "7%" }}></th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </>
-      )}
-    </main>
+              </thead>
+              <tbody>
+                {contacts.map((contact, index) => (
+                  <tr
+                    key={contact.id}
+                    style={
+                      index % 2 == 0 ? { backgroundColor: "#e3eefa" } : null
+                    }
+                  >
+                    <td style={{ width: "8%" }}>{index + 1}</td>
+                    <td style={{ width: "23%" }}>{contact.name}</td>
+                    <td style={{ width: "24%" }}>
+                      {contact.phone} - {contact.email}
+                    </td>
+                    <td style={{ width: "38%" }}>{contact.about}</td>
+                    <td style={{ width: "7%" }}>
+                      <img
+                        style={{ float: "left" }}
+                        src={editIcon}
+                        onClick={() => handleEdit(contact.id)}
+                      ></img>
+                      <img
+                        style={{ float: "right" }}
+                        src={deleteIcon}
+                        onClick={() => handleDelete(contact.id)}
+                      ></img>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        )}
+        <button onClick={handleAdd}>Add +</button>
+      </>
+    </div>
   );
 }
 
